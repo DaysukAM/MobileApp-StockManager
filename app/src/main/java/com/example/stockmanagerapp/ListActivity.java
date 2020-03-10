8dbb036199e8d6695bd839b1d6c3cbafe33545c0
Anthony Moutonnet <anthony.moutonnet@autoliv.com>
2020-03-10 09:45:41 +0100

    first fonction

--- app/src/main/java/com/example/stockmanagerapp/ListActivity.java
+++ app/src/main/java/com/example/stockmanagerapp/ListActivity.java
@@ -3,12 +3,30 @@ package com.example.stockmanagerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
+
+    private TextView count;
+    private Button buttoncount;
+    private int clicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        this.count = (TextView) findViewById(R.id.count);
+        this.buttoncount = (Button) findViewById(R.id.buttoncount);
+
+        buttoncount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clicks++;
+                count.setText("clicks: " + clicks);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/stockmanagerapp/MainActivity.java
+++ app/src/main/java/com/example/stockmanagerapp/MainActivity.java
@@ -9,14 +9,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button play;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.play =findViewById(R.id.play);
+        Button play = findViewById(R.id.play);
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
