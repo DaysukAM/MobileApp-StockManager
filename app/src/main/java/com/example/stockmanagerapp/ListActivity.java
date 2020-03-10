f37a92aa4e4c124448544270104526efda6395b6
Anthony Moutonnet <anthony.moutonnet@autoliv.com>
2020-03-10 11:18:56 +0100

    get methode implementation

--- app/src/main/java/com/example/stockmanagerapp/ListActivity.java
+++ app/src/main/java/com/example/stockmanagerapp/ListActivity.java
@@ -2,15 +2,28 @@ package com.example.stockmanagerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class ListActivity extends AppCompatActivity {
 
     private TextView count;
     private Button buttoncount;
+    private Button backbutton;
     private int clicks = 0;
 
     @Override
@@ -18,15 +31,49 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        String URL="http://stockmanager.test:8080/materials";
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest=new JsonObjectRequest(
+        Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void  onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
         this.count = (TextView) findViewById(R.id.count);
         this.buttoncount = (Button) findViewById(R.id.buttoncount);
 
         buttoncount.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            clicks++;
+            count.setText("clicks: " + clicks);
+        }
+    });
+        this.backbutton = (Button) findViewById(R.id.backbutton);
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                clicks++;
-                count.setText("clicks: " + clicks);
+                Intent otherActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(otherActivity);
+                finish();
             }
         });
-    }
+}
 }
