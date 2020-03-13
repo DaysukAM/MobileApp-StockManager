21df8894406f33a77db5e432b0958f5316bd15bb
Anthony Moutonnet <anthony.moutonnet@autoliv.com>
2020-03-13 16:48:07 +0100

    register with api fonctionnal

--- app/src/main/java/com/example/stockmanagerapp/RegisterActivity.java
+++ app/src/main/java/com/example/stockmanagerapp/RegisterActivity.java
@@ -1,19 +1,34 @@
 package com.example.stockmanagerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.regex.Pattern;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText editName, editMail, editPassword;
     private Button submitButton;
+    private Button buttonLoginLink;
+    private TextView textViewLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +46,49 @@ public class RegisterActivity extends AppCompatActivity {
                 userSignUp();
             }
         });
+
+        buttonLoginLink = (Button) findViewById(R.id.buttonLoginLink);
+        buttonLoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
+    Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://10.66.24.112/StockManager/server.php/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build();
+
+
+    StockManagerApi stockManagerApi = retrofit.create(StockManagerApi.class);
+
 
     private void userSignUp(){
+        textViewLog = findViewById(R.id.textViewLog);
         String name = editName.getText().toString().trim();
-        String mail = editMail.getText().toString().trim();
+        String email = editMail.getText().toString().trim();
         String password = editPassword.getText().toString().trim();
 
-        if (mail.isEmpty()){
+        if (name.isEmpty()){
+            editName.setError("indiquez un nom");
+            editName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()){
             editMail.setError("indiquez une adresse e-mail !");
             editMail.requestFocus();
             return;
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             editMail.setError("indiquez une adresse e-mail valide !");
             editMail.requestFocus();
             return;
@@ -62,14 +106,25 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
-        if (name.isEmpty()){
-            editName.setError("indiquez un nom");
-            editName.requestFocus();
-            return;
-        }
 
-        /* do user registration */
 
-        
+        Call<ResponseBody> call = stockManagerApi.createUser(name, email, password);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    textViewLog.setText("Création de l'utilisateur " + name + " réussi");
+                if(!response.isSuccessful()) {
+                    textViewLog.setText("code: " + response.code());
+                    return;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                textViewLog.setText(t.getMessage());
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/stockmanagerapp/StockManagerApi.java
+++ app/src/main/java/com/example/stockmanagerapp/StockManagerApi.java
@@ -2,9 +2,12 @@ package com.example.stockmanagerapp;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
@@ -17,9 +20,21 @@ public interface StockManagerApi {
     @GET("materials")
     Call<List<Materials>> getMaterials();
 
+    @FormUrlEncoded
+    @POST("user/create")
+    Call<ResponseBody> createUser(
+            @Field("name") String name,
+            @Field("email") String email,
+            @Field("password") String password
+
+    );
+
     @POST("materials/create")
     Call<Materials> createMaterial(@Body Materials materials);
 
+
     @DELETE("materials/delete/{id}")
     Call<Void> deleteMaterials(@Path("id") int id);
+
+
 }
